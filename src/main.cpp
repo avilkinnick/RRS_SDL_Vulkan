d97aba9d1aa1b3761ae035f8f7526d99a999fa58
avilkinnick <avilkin.nick@gmail.com>
2024-12-09 16:12:14 +0300

    Basic exception handling

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,24 @@
+#include <exception>
+
 #include <SDL_main.h>
+
+#include "logging.h"
 
 int main(int argc, char* argv[])
 {
+    try
+    {
+    }
+    catch (const std::exception& exception)
+    {
+        log_error(exception.what());
+        return 1;
+    }
+    catch (...)
+    {
+        log_error("Unknown exception");
+        return 1;
+    }
+
     return 0;
 }
