c7b9eb556b513045df6b8d262790abe3288e8207
avilkinnick <avilkin.nick@gmail.com>
2024-12-09 15:55:49 +0300

    Add logging

---
+++ src/logging.cpp
@@ -0,0 +1,25 @@
+#include "logging.h"
+
+#include <cstdarg>
+#include <cstdio>
+
+#include <SDL_error.h>
+
+#define LOG_IMPLEMENTATION(category, ansi_escape_code) \
+    void log_##category(const char* format, ...) \
+    { \
+        std::va_list args; \
+        va_start(args, format); \
+        std::fprintf(stderr, ansi_escape_code); \
+        std::vfprintf(stderr, format, args); \
+        std::fprintf(stderr, "\033[0m\n"); \
+        va_end(args); \
+    }
+
+LOG_IMPLEMENTATION(warning, "\033[1;33m")
+LOG_IMPLEMENTATION(error, "\033[1;31m")
+
+void log_sdl_error()
+{
+    log_error("SDL error: %s", SDL_GetError());
+}
