9302e0b63ab9e0b9a412c1ba606eedec3d64c0b4
avilkinnick <avilkin.nick@gmail.com>
2024-12-19 17:14:52 +0300

    Added CLI11

--- include/simulator/Application.h
+++ include/simulator/Application.h
@@ -5,6 +5,8 @@ class Application
 {
 public:
     Application(int argc, char* argv[]);
+
+    int parse_command_line_arguments(int argc, char* argv[]);
 };
 
 #endif // SIMULATOR_APPLICATION_H
--- src/simulator/Application.cpp
+++ src/simulator/Application.cpp
@@ -1,5 +1,39 @@
 #include "simulator/Application.h"
 
+#include "Logger.h"
+
+#include <CLI11.hpp>
+
+#include <stdexcept>
+#include <string>
+
 Application::Application(int argc, char* argv[])
 {
+    std::string command_line_string;
+    for (int i = 1; i < argc; ++i)
+    {
+        command_line_string = command_line_string + ' ' + argv[i];
+    }
+
+    Logger& logger = Logger::get_instance();
+    logger.log_info("Simulator started with command line:%s", command_line_string.c_str());
+    logger.log_info("Started initialization");
+
+    int parse_command_line_arguments_result = parse_command_line_arguments(argc, argv);
+    if (parse_command_line_arguments_result != 0)
+    {
+        throw std::runtime_error(
+            std::string("Command line parser failed with error code ")
+            + std::to_string(parse_command_line_arguments_result)
+        );
+    }
+}
+
+int Application::parse_command_line_arguments(int argc, char* argv[])
+{
+    CLI::App app("RRS simulator");
+    argv = app.ensure_utf8(argv);
+
+    CLI11_PARSE(app, argc, argv);
+    return 0;
 }
